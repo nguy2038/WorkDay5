a410c489d4094150944674366b93e430afcfbafa
idsc3101 <idsc3101@gmail.com>
2013-10-07 15:05:36 -0500

    Done

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -9,7 +9,9 @@ Public Class Form1
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+        Dim firstspace As Integer
+        firstspace = name.IndexOf(" ")
+        Return name.Substring(0, firstspace)
     End Function
 
 End Class
